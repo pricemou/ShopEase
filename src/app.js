b2146b71f87659565c2f6a448eb650d6e3e56896
pricemou <scameleons3@gamil.com>
2025-05-19 14:27:24 -0400

    initiallisation

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// $(".main-slider").slick({
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//     lazyLoad: "progressive",
+//     speed: 600,
+//     arrows: false,
+//     dots: true,
+//     cssEase: "cubic-bezier(0.87, 0.03, 0.41, 0.9)",
+//     responsive: [
+//       { breakpoint: 768, settings: { dots: false } } // Optional: Disable dots on small screens
+//     ]
+//   });
+  
+alert("hh");
+console.log("jddffd");
